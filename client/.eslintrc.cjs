f72f319b0380f86eaaa0b155bbb779e91ad83800
Nguyen Van Tien Hai <hainvthe172670@fpt.edu.vn>
2025-08-19 21:30:44 +0700

    second commit

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -19,7 +19,7 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh", "@typescript-eslint"],
   rules: {
-    quotes: ["error", "single", { avoidEscape: false }],
+    quotes: "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
