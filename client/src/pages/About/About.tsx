61372daec84c1a753e9f98b74606972bf12a9356
NguyenBaHa08052003 <nguyenbaha0805@gmail.com>
2025-08-20 16:14:24 +0700

    [hanb/20-8] config shadcn-ui

--- client/src/pages/About/About.tsx
+++ client/src/pages/About/About.tsx
@@ -1,8 +1,12 @@
 import "./About.scss";
+import { Button } from "@/components/ui/button"
 const About = () => {
   return (
     <div>
       <h1>About</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
     </div>
   );
 };
