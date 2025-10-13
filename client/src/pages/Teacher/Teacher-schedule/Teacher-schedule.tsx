b61e61d4d2900c2e0db4363220fe6751cbe35c2e
Nguyen Van Tien Hai <nvthai061105@gmail.com>
2025-10-13 19:17:30 +0700

    fix

--- client/src/pages/Teacher/Teacher-schedule/Teacher-schedule.tsx
+++ client/src/pages/Teacher/Teacher-schedule/Teacher-schedule.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { useEffect, useMemo, useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
