7886645f4e3bf8417900f749563e821d7550126b
NguyenBaHa08052003 <nguyenbaha0805@gmail.com>
2025-10-21 22:22:19 +0700

    [hanb/21-10]base send email absent

---
+++ server/src/modules/shared/controllers/email-notification.controller.ts
@@ -0,0 +1,7 @@
+import { Body, Controller } from "@nestjs/common";
+import { EmailNotificationService } from "../services/email-notification.service";
+
+@Controller('email-notifications')
+export class EmailNotificationController {
+    
+}
